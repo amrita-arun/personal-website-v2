e98f68151eaabb1b55bd93bbd4feaa486ebc8486
amrita-arun <amrita.arun2013@gmail.com>
2026-03-16 17:13:40 -0700

    fixed deployment issue

--- personal-website/components/Hero.tsx
+++ personal-website/components/Hero.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { Github, Linkedin, Mail, Twitter } from 'lucide-react';
+import { Github, Linkedin, Twitter } from 'lucide-react';
 import { motion } from 'framer-motion';
 import { Chip } from '@/components/ui/chip';
 
